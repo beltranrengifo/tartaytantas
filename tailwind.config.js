b77003e133b2fd903708430631318ba374682024
Beltrán Rengifo <b.rengifo@docline.es>
2021-01-31 13:47:00 +0100

    fix(tailwind): new purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,18 @@ module.exports = {
     purgeLayersByDefault: true,
   },
   purge: {
-    enabled: true,
-    content: ['./**/*.vue', './**/*.ts', './**/*.js'],
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+      // TypeScript
+      'plugins/**/*.ts',
+      'content/**/*.ts',
+      'nuxt.config.ts',
+    ],
   },
   theme: {
     fontFamily: {
