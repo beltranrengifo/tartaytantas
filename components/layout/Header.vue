91f8eedaa820e65dcb5b77e9de4863277ce0b6fa
Beltrán Rengifo <b.rengifo@docline.es>
2021-02-11 19:56:32 +0100

    fix: mqs

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -2,7 +2,7 @@
   <header
     class="fixed w-full top-0 p-5 flex items-center flex-col sm:flex-row sm:justify-between flex-shrink-0 z-20"
   >
-    <Logo class="self-center sm:self-auto mb-4 sm:m-0" />
+    <Logo class="self-auto m-0 hidden sm:block" />
     <Navigation
       class="hidden desktop-menu:flex self-center sm:self-auto mb-4 sm:m-0"
     />
