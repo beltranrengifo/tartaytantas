ac84149a328137be045996c0a41f52c6a0b75fb0
Beltrán Rengifo <b.rengifo@docline.es>
2021-02-11 11:30:02 +0100

    fix: cleanup

--- mixins/parallax.vue
+++ mixins/parallax.vue
@@ -11,41 +11,6 @@ export default Vue.extend({
 
   mounted(): void {
     window.addEventListener('scroll', this.handleScroll)
-    window.onscroll = () => {
-      // Movile devices horizontal transitions
-      /* if (isInViewport(document.querySelector('.features'))) {
-        mobiles[0].style.transform = `translateX(${
-          mobiles[0].getBoundingClientRect().top * 0.2
-        }px)`
-        mobiles[1].style.transform = `translateX(${
-          mobiles[1].getBoundingClientRect().top * 0.07
-        }px)`
-      } */
-      // Message box transitions
-      /* if (isInViewport(document.querySelector('.facts'))) {
-        messages[0].style.transform = `translate(${
-          -messages[0].getBoundingClientRect().top * 0.2
-        }px, ${messages[0].getBoundingClientRect().top * 0.1}px)`
-        messages[1].style.transform = `translate(${
-          messages[1].getBoundingClientRect().top * 0.1
-        }px, ${messages[1].getBoundingClientRect().top * 0.05}px)`
-        messages[2].style.transform = `translate(${
-          messages[2].getBoundingClientRect().top * 0.3
-        }px, ${-messages[2].getBoundingClientRect().top * 0.05}px)`
-      } */
-      // Slider transition
-      /* if (isInViewport(document.querySelector('.join'))) {
-        sliderTrack.style.transform = `translateX(${
-          -document.querySelector('.join').getBoundingClientRect().top * 0.4
-        }px)`
-      } */
-      // Isometric slider transition
-      /* if (isInViewport(document.querySelector('.dark-background'))) {
-        bgDark.classList.add('on')
-      } else {
-        bgDark.classList.remove('on')
-      } */
-    }
   },
 
   methods: {
