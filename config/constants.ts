af473d76ba64c1cf21d4d79d990201cf99783d79
Beltran Rengifo <beltran.rengifo@liferay.com>
2022-06-03 09:28:08 +0200

    feat: add new shop link

--- config/constants.ts
+++ config/constants.ts
@@ -1,3 +1,6 @@
 export const SHOW_MENU_SCROLL_THRESHOLD: number = 50
 export const GOOGLE_PAGE: string = 'https://g.page/tartaytantas'
-export const SHOP_URL: string = 'https://tienda-online.tartaytantas.es/'
+export const SHOP_URL: string =
+  'https://www.portalrest.com/index.html?data=%3DATPrNXYmETPtZyMxMzN5ETP0NXZSRWa'
+export const SHOP_URL_APANYMANTEL: string =
+  'https://tienda-online.tartaytantas.es/'
