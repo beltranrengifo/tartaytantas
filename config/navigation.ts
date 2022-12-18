1027c2a56c7fb1ef2accbac8283c08a25fca574b
Beltrán Rengifo <merloc@gmail.com>
2022-12-18 21:18:11 +0100

    change menu text

--- config/navigation.ts
+++ config/navigation.ts
@@ -2,7 +2,7 @@ import { SHOP_URL } from '@/config/constants'
 
 export default [
   { name: 'Inicio', path: '/', target: '_self' },
-  { name: 'Del obrador', path: '/tartas', target: '_self' },
+  { name: 'Tartas', path: '/tartas', target: '_self' },
   {
     name: 'Tienda',
     path: SHOP_URL,
