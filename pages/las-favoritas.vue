e04244a88cf1e5cf248990b6c88b617079911db2
Beltrán Rengifo <merloc@gmail.com>
2023-02-04 14:17:19 +0100

    feat: add las favoritas page and media

---
+++ pages/las-favoritas.vue
@@ -0,0 +1,37 @@
+<template>
+  <section-renderer :sections="landing" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import MetaData from '@/mixins/meta-data'
+
+import { landing } from '@/content'
+
+export default Vue.extend({
+  name: 'Landing',
+
+  mixins: [MetaData],
+
+  data() {
+    return {
+      landing,
+    }
+  },
+
+  asyncData(): object {
+    return {
+      landing,
+    }
+  },
+
+  head(): object {
+    return (this as any).getMetadata({
+      title:
+        'Tartaytantas | Las tartas favoritas de nuestros clientes de siempre',
+      description:
+        'Somos un pequeño obrador de tartas inspiradas en recetas clásicas, con referencias a la pastelería americana de mediados del S XX.',
+    })
+  },
+})
+</script>
