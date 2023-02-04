9532677365ecba2fe57f3cd1e1fdbabc160fea5a
Beltrán Rengifo <merloc@gmail.com>
2023-02-04 20:50:29 +0100

    feat: whatsapp url as constant

--- config/constants.ts
+++ config/constants.ts
@@ -2,3 +2,5 @@ export const SHOW_MENU_SCROLL_THRESHOLD: number = 50
 export const GOOGLE_PAGE: string = 'https://g.page/tartaytantas'
 export const SHOP_URL: string =
   'https://www.portalrest.com/index.html?data=%3DATPrNXYmETPtZyMxMzN5ETP0NXZSRWa'
+export const WHATSAPP_URL: string =
+  'https://wa.me/34663398161?text=Quiero una tarta! :D'
