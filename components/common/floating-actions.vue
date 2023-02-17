523ac46d781060132e1379d194984bc74c9e373c
Beltrán Rengifo <merloc@gmail.com>
2023-02-17 21:00:57 +0100

    feat: better actions in its own component

---
+++ components/common/floating-actions.vue
@@ -0,0 +1,118 @@
+<template>
+  <span>
+    <floating-action
+      :link="PHONE_CALL_URL"
+      :position="{ bottom: '96px', right: '24px' }"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        viewBox="0,0,256,256"
+        width="50px"
+        height="50px"
+        fill-rule="nonzero"
+      >
+        <g transform="">
+          <g
+            fill="#b82529"
+            fill-rule="nonzero"
+            stroke="none"
+            stroke-width="1"
+            stroke-linecap="butt"
+            stroke-linejoin="miter"
+            stroke-miterlimit="10"
+            stroke-dasharray=""
+            stroke-dashoffset="0"
+            font-family="none"
+            font-weight="none"
+            font-size="none"
+            text-anchor="none"
+            style="mix-blend-mode: normal"
+          >
+            <path d="M0,256v-256h256v256z" id="bgRectangle"></path>
+          </g>
+          <g
+            fill-opacity="0"
+            fill="#000000"
+            fill-rule="nonzero"
+            stroke-opacity="0"
+            stroke="#000000"
+            stroke-width="30.72"
+            stroke-linecap="butt"
+            stroke-linejoin="round"
+            stroke-miterlimit="10"
+            stroke-dasharray=""
+            stroke-dashoffset="0"
+            font-family="none"
+            font-weight="none"
+            font-size="none"
+            text-anchor="none"
+            style="mix-blend-mode: normal"
+          >
+            <path
+              d="M184.32,20.43c28.21803,0 51.2,22.98197 51.2,51.2v112.64c0,28.21803 -22.98197,51.2 -51.2,51.2h-112.64c-28.21803,0 -51.2,-22.98197 -51.2,-51.2v-112.64c0,-28.21803 22.98197,-51.2 51.2,-51.2z"
+              id="strokeMainSVG"
+            ></path>
+          </g>
+          <g
+            fill="#ffffff"
+            fill-rule="nonzero"
+            stroke="none"
+            stroke-width="1"
+            stroke-linecap="butt"
+            stroke-linejoin="miter"
+            stroke-miterlimit="10"
+            stroke-dasharray=""
+            stroke-dashoffset="0"
+            font-family="none"
+            font-weight="none"
+            font-size="none"
+            text-anchor="none"
+            style="mix-blend-mode: normal"
+          >
+            <g transform="scale(5.12,5.12)">
+              <path
+                d="M14,3.99023c-5.51133,0 -10,4.48867 -10,10v22c0,5.51133 4.48867,10 10,10h22c5.51133,0 10,-4.48867 10,-10v-22c0,-5.51133 -4.48867,-10 -10,-10zM18.00586,12.0332c0.628,0.027 1.20473,0.38083 1.55273,0.92383c0.396,0.618 1.01055,1.57712 1.81055,2.82813c0.73,1.141 0.78091,2.61469 0.12891,3.80469l-1.46484,2.08398c-0.396,0.564 -0.47498,1.28588 -0.20898,1.92188c0.414,0.99 1.21658,2.5115 2.64258,3.9375c1.426,1.426 2.9475,2.22858 3.9375,2.64258c0.636,0.266 1.35788,0.18702 1.92188,-0.20898l2.08398,-1.46484c1.19,-0.652 2.66369,-0.60009 3.80469,0.12891c1.251,0.8 2.21012,1.41455 2.82813,1.81055c0.543,0.348 0.89683,0.92473 0.92383,1.55273c0.154,3.564 -2.60716,5.00781 -3.41016,5.00781c-0.556,0 -7.2403,0.7597 -14.7793,-6.7793c-7.539,-7.539 -6.7793,-14.2233 -6.7793,-14.7793c0,-0.803 1.44381,-3.56416 5.00781,-3.41016z"
+              ></path>
+            </g>
+          </g>
+        </g>
+      </svg>
+    </floating-action>
+    <floating-action
+      :link="WHATSAPP_URL"
+      :position="{ bottom: '34px', right: '24px' }"
+    >
+      <svg
+        class="w-4 inline"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+      >
+        <path
+          d="M256.064 0h-.128C114.784 0 0 114.816 0 256c0 56 18.048 107.904 48.736 150.048l-31.904 95.104 98.4-31.456C155.712 496.512 204 512 256.064 512 397.216 512 512 397.152 512 256S397.216 0 256.064 0z"
+          fill="#4caf50"
+        />
+        <path
+          d="M405.024 361.504c-6.176 17.44-30.688 31.904-50.24 36.128-13.376 2.848-30.848 5.12-89.664-19.264-75.232-31.168-123.68-107.616-127.456-112.576-3.616-4.96-30.4-40.48-30.4-77.216s18.656-54.624 26.176-62.304c6.176-6.304 16.384-9.184 26.176-9.184 3.168 0 6.016.16 8.576.288 7.52.32 11.296.768 16.256 12.64 6.176 14.88 21.216 51.616 23.008 55.392 1.824 3.776 3.648 8.896 1.088 13.856-2.4 5.12-4.512 7.392-8.288 11.744-3.776 4.352-7.36 7.68-11.136 12.352-3.456 4.064-7.36 8.416-3.008 15.936 4.352 7.36 19.392 31.904 41.536 51.616 28.576 25.44 51.744 33.568 60.032 37.024 6.176 2.56 13.536 1.952 18.048-2.848 5.728-6.176 12.8-16.416 20-26.496 5.12-7.232 11.584-8.128 18.368-5.568 6.912 2.4 43.488 20.48 51.008 24.224 7.52 3.776 12.48 5.568 14.304 8.736 1.792 3.168 1.792 18.048-4.384 35.52z"
+          fill="#fafafa"
+        />
+      </svg>
+    </floating-action>
+  </span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { WHATSAPP_URL, PHONE_CALL_URL } from '@/config/constants'
+
+export default Vue.extend({
+  name: 'FloatingActions',
+
+  data() {
+    return {
+      WHATSAPP_URL,
+      PHONE_CALL_URL,
+    }
+  },
+})
+</script>
