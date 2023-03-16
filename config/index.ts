3012c947b6302e1216b61831a00822e7fd308ea1
Beltrán Rengifo <merloc@gmail.com>
2023-03-16 21:00:25 +0100

    feat: use vacations days

--- config/index.ts
+++ config/index.ts
@@ -2,3 +2,4 @@ import navigation from './navigation'
 import { SHOW_MENU_SCROLL_THRESHOLD } from './constants'
 
 export { navigation, SHOW_MENU_SCROLL_THRESHOLD }
+export * from './vacation'
---
+++ config/vacation.ts
@@ -0,0 +1,5 @@
+/*
+ * IMPORTANT
+ * for each date use this format: YYYY-MM-DD
+ */
+export const vacations = ['2023-04-06', '2023-04-07', '2023-04-08']
