1ac2fe9f1ec2ff1ff1735824cc1429c3ae7bd338
Beltrán Rengifo <merloc@gmail.com>
2023-03-31 19:32:28 +0200

    feat: remove portalrest

--- config/navigation.ts
+++ config/navigation.ts
@@ -1,13 +1,6 @@
-import { SHOP_URL } from '@/config/constants'
-
 export default [
   { name: 'Inicio', path: '/', target: '_self' },
   { name: 'Tartas', path: '/tartas', target: '_self' },
-  {
-    name: 'Tienda',
-    path: SHOP_URL,
-    target: '_blank',
-  },
   { name: 'Las favoritas', path: '/las-favoritas', target: '_self' },
   { name: 'Nosotras', path: '/nosotros', target: '_self' },
 ]
