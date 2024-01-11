bcad4a06d6833d6166bca202b486bdbdacec9263
Beltrán Rengifo <merloc@gmail.com>
2024-01-11 21:51:14 +0100

    navigation use cleanup. closer to use

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,2 @@
-import navigation from './navigation'
-import { SHOW_MENU_SCROLL_THRESHOLD } from './constants'
-
-export { navigation, SHOW_MENU_SCROLL_THRESHOLD }
-export * from './vacation'
+export { SHOW_MENU_SCROLL_THRESHOLD } from './constants'
+export { vacations } from './vacation'
--- config/navigation.ts
+++
@@ -1,6 +0,0 @@
-export default [
-  { name: 'Inicio', path: '/', target: '_self' },
-  { name: 'Tartas', path: '/tartas', target: '_self' },
-  { name: 'Las favoritas', path: '/las-favoritas', target: '_self' },
-  { name: 'Nosotras', path: '/nosotros', target: '_self' },
-]
