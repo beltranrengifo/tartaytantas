69e9723869383ade3b242659f4b87d237eb990bf
Beltrán Rengifo <merloc@gmail.com>
2025-06-28 13:00:54 +0200

    fix: legal page error. legal texts

--- pages/legal.vue
+++ pages/legal.vue
@@ -5,11 +5,16 @@
 <script lang="ts">
 import Vue from 'vue'
 import MetaData from '@/mixins/meta-data'
+import SectionRenderer from '@/components/content/section-renderer.vue'
 
 import { legal } from '@/content'
 
 export default Vue.extend({
   name: 'Legal',
+
+  components: {
+    SectionRenderer,
+  },
 
   mixins: [MetaData],
 
